fc3ef501106c7735a8e474f25bdb0992593299d7
psdoood <aydeniswright@gmail.com>
2025-01-03 17:11:29 -0700

    Added a camera with a basic plane to move around.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include "inc/raylib.h"
+#include "inc/rcamera.h"
 
 int main() {
     const int screenWidth = 800;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Window");
 
+    // Camera settings
+    Camera camera = {0};
+    camera.position = (Vector3){0.0f, 2.0f, 4.0f};
+    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          
+    camera.fovy = 60.0f;                             
+    camera.projection = CAMERA_PERSPECTIVE;   
+    int cameraMode = CAMERA_FIRST_PERSON;
+
+    DisableCursor();
+
     // Game loop
     while (!WindowShouldClose()) {
+        UpdateCamera(&camera, cameraMode);
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("TEST", 10, 10, 20, DARKGRAY);
+            ClearBackground(BLACK);
+            BeginMode3D(camera);
+                DrawPlane((Vector3){0.0f, -0.0f, 0.0f}, (Vector2){40.0f, 40.0f}, WHITE);
+                DrawGrid(40, 1);
+            EndMode3D();
         EndDrawing();
     }
 
