982a94ad67d9694d005e3135aca9d0ae69066b83
psdoood <aydeniswright@gmail.com>
2025-01-04 18:30:26 -0700

    Added clean condition before building.

--- buildrun.bat
+++ buildrun.bat
@@ -1,5 +1,8 @@
 @echo off
-g++ src/main.cpp -o my_program.exe -I./inc -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm -std=c++17
+if exist my_program.exe (
+    del my_program.exe
+)
+g++ src/main.cpp src/player.cpp -o my_program.exe -I./inc -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm -std=c++17
 
 if exist my_program.exe (
     echo Running the game...
